ddf4e36303d5aad2558b67312ec3ebf3a724bdbe
Tai_PC <tai@chiliman.com.tw>
2017-10-17 19:23:24 +0800

    1017_2
    1017_2

--- SystemMaintain/CommonPage/header_logo_user.php
+++
@@ -1,9 +0,0 @@
-    	<div class="logoAndUser">
-        	<h1><img src="../images/cross_think_logo.gif" alt="Cross Think" width="296" height="72"></h1>
-            <ul>
-            	<li class="name"><span class="sized-text"><?php echo $USER_NM;?></span></li><!-- 使用者 -->
-                <li class="btn"><button type="button" onclick="javascript:location.href='/SystemMaintain/ChangePWD/ChangePWD_Modify.php'"><span class="sized-text">變更密碼</span></button></li><!-- 如果沒有，就不寫入這個LI -->
-                <li class="btn"><button type="button" onclick="javascript:location.href='/SystemMaintain/Menu/Member_Logout.php'"><span class="sized-text">LOG OUT</span></button></li><!-- 如果沒有，就不寫入這個LI -->
-            </ul>
-        </div>
-        <div class="clearFloat"></div>
--- SystemMaintain/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 	
-include_once($_SERVER['DOCUMENT_ROOT'] . "/config.ini.php");
-
-	$_SESSION["M_USER_ID"] = '';
-	$_SESSION["M_USER_NM"] = '';
-	
-	header("Location:/SystemMaintain/Menu/MemberLogin.php"); 
-	
-?>
